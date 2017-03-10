6ff09d00fa8c9249e5c300aa70cc596ebd951447
brighttang2016 <798758642@qq.com>
2017-03-10 17:46:28 +0800

    添加阿里云邮件、短信推送

--- tlms-web/src/main/java/com/sys/aop/aspect/MyException.java
+++ tlms-web/src/main/java/com/sys/aop/aspect/MyException.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 
+ * @author tom 2017-03-10
+ */
 @ControllerAdvice
 @ResponseBody
 public class MyException {
-
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  
+	@ResponseStatus(HttpStatus.OK)  
 	@ExceptionHandler(Exception.class)  
 	public String handleException(Exception e){
-		System.out.println("handleException5555555555");
 		return e.getMessage();
 	}
 }
